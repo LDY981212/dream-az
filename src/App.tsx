1ee468ee74329e0a755748d57a0c7e03ec36d477
unknown <d653@naver.com>
2025-11-06 17:53:40 +0900

    Feat - init setting

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
